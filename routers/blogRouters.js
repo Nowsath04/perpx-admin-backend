b5f1946615a95f6ebb12a3962cf3bd3d585caecd
Nowsath04 <nowsathnichu@gmail.com>
2023-12-14 11:11:53 +0530

    add

--- routers/blogRouters.js
+++ routers/blogRouters.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const { NewBlog, GetSingleBlog, DeleteSingleBlog, UpdateSingleBlog, getAllBlog, GetSingleBlogUrl } = require("../controllers/blogControllers");
-const jwtVerification = require("../middleware/jwtVerfication");
+const jwtVerification = require("../middleware/JwtVerfication");
 const upload = require("../utils/fileUpload");
 
 
